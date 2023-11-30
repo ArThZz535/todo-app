ea96e25ae142ca0260b686a05b6f3ba6de815304
Arthur Rodrigues de Sousa Araújo <arthur.rodriguess.sousa@gmail.com>
2023-11-29 22:43:03 -0300

    COMMIT AUTONOMO ALINHAMENTO DE CÓDIGO

--- public/js/script.js
+++ public/js/script.js
@@ -1,60 +1,78 @@
 function completarTarefa (id) {
+
     fetch("http://localhost:3000/completar", {
+
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id })
+
     })
 
     window.location.reload()
+
 }
+
+
 
 function descompletarTarefa (id) {
+
     fetch("http://localhost:3000/descompletar", {
+
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id })
+
     })
 
     window.location.reload()
+
 }
+
+
 
 function excluirTarefa(id) {
+    
     fetch("http://localhost:3000/excluir", {
+
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id })
+
     })
+
 }
 
 
-function alterarTema()  {
+
+function alterarTema() {
+
     const tema = localStorage.getItem("tema")
     const body = document.querySelector("body")
     const button = document.querySelector(".tema-button")
 
     if (tema) { 
+
         let novoTema
 
         if (tema === "light") {
+
             novoTema = "dark"
             button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Ícone do Sol">`
             body.classList.remove("light")
             body.classList.add("dark")
+
         } else {
+
             novoTema = "light"
             button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Ícone da Lua">`
             body.classList.remove("dark")
             body.classList.add("light")
+
         }
 
         localStorage.setItem("tema", novoTema)
         return
+
     }
 
     localStorage.setItem("tema", "dark")
@@ -62,20 +80,29 @@ function alterarTema()  {
 
 }
 
+
+
 function verificarTema() {
+
     const tema = localStorage.getItem("tema")
     const body = document.querySelector("body")
     const button = document.querySelector(".tema-button")
 
-    
     if (tema) {
+
         if (tema === "dark")
         body.classList.add("dark")
         button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Ícone do Sol">`
+
     } else {
+
         body.classList.add("light")
         button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Ícone da Lua">`
+
     }
+
 }
+
+
 
 verificarTema()
